5524657a9b7d5abde0355a225131e45dfa548669
Wesley Hales <wesleyhales@gmail.com>
2012-08-09 10:31:53 -0400

    trying to create a queue

--- src/main/java/com/onslyde/service/PerformanceService.java
+++ src/main/java/com/onslyde/service/PerformanceService.java
@@ -1,9 +1,11 @@
 package com.onslyde.service;
 
 import com.onslyde.util.ClientEvent;
+import com.onslyde.util.SimpleQueue;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
@@ -15,27 +17,45 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLDecoder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Path("/performance")
 @RequestScoped
 @Stateful
 public class PerformanceService {
 
+    @Inject
+    SimpleQueue simpleQueue;
+
     @GET
     @Path("/go")
     @Produces("text/html")
-    public String go(@QueryParam("url") String url) {
-        String param1AfterDecoding = "";
+    public String go(@QueryParam("url") String url, @QueryParam("cached") String cached) {
+         Map<String,String> tempMap = new HashMap<String,String>();
+         tempMap.put("url",url);
+         tempMap.put("cached",cached);
+         simpleQueue.put(tempMap);
+         return runTest();
+    }
+    private String runTest(){
+        boolean running = false;
+        while(true){
+
+        if(!simpleQueue.isEmpty() && !running){
+            running = true;
+        Map<String,String> tempMap = (HashMap)simpleQueue.get();
+        String url = tempMap.get("url");
+        String cached = tempMap.get("cached");
+
         UUID random = UUID.randomUUID();
+        String taskName = "performance";
 //        try {
 //            param1AfterDecoding = URLDecoder.decode(url, "UTF-8");
 //            System.out.println(param1AfterDecoding);
 //        } catch (UnsupportedEncodingException e) {
 //            e.printStackTrace();
 //        }
+        //System.out.println("---" + url);
         boolean keepgoing = false;
         try {
             URL urltemp = new URL(url);
@@ -51,33 +71,39 @@ public class PerformanceService {
         }
         Response.ResponseBuilder builder = null;
         if(keepgoing){
+            if(cached.equals("true")){
+                taskName = "performancecache";
+            }
         try
         {
 
-            for(int i = 0; i < 5; i++) {
-                Process p=Runtime.getRuntime().exec("phantomjs --disk-cache=no confess-mod.js "+ url +" performance json " + random.toString() );
+            for(int i = 0; i <= 5; i++) {
+                Process p=Runtime.getRuntime().exec("phantomjs --disk-cache=no confess-mod.js "+ url +" "+taskName+" json " + random.toString() );
                 //p.waitFor();
                 BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream()));
                 String line=reader.readLine();
                 while(line!=null)
                 {
-                    System.out.println(line);
+                //    System.out.println(line);
                     line=reader.readLine();
                 }
             }
         }
-        catch(IOException e1) {}
+        catch(IOException e1) {return "#fail";}
         //catch(InterruptedException e2) {}
 
             System.out.println("Done");
+            running = false;
         }else{
             System.out.println("Bad URL");
-            return "failed, sorry! check the url";
+            return "#fail";
         }
-        builder = Response.ok();
+
 
         //return builder.build();
         return "/rest/performance/report?uuid=" + random.toString();
+        }
+        }
 }
 
 
@@ -91,7 +117,14 @@ public class PerformanceService {
         builder = Response.ok();
         String all = "";
         try {
-            BufferedReader in = new BufferedReader(new FileReader("/Users/wesleyhales/www/jboss-as-7.1.1.Final/confess-report-" + uuid + ".json"));
+            BufferedReader in;
+            File locatedFile = new File("/Users/wesleyhales/www/jboss-as-7.1.1.Final/reports/confess-report-" + uuid + ".json");
+            if(locatedFile.exists()) {
+                in = new BufferedReader(new FileReader("/Users/wesleyhales/www/jboss-as-7.1.1.Final/reports/confess-report-" + uuid + ".json"));
+            }else{
+                return "#fail";
+            }
+
             String ln;
 
             while ((ln = in.readLine()) != null)
---
+++ src/main/java/com/onslyde/util/SimpleQueue.java
@@ -0,0 +1,76 @@
+package com.onslyde.util;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Singleton;
+import javax.ejb.Stateful;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+import java.io.Serializable;
+import java.util.LinkedList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wesleyhales
+ * Date: 8/8/12
+ * Time: 8:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Dependent
+public class SimpleQueue<E> implements Serializable {
+
+    private LinkedList<E> list = new LinkedList<E>();
+
+    /**
+     * Puts object in queue.
+     */
+    public void put(E o) {
+        list.addLast(o);
+    }
+
+    /**
+     * Returns an element (object) from queue.
+     *
+     * @return element from queue or <code>null</code> if queue is empty
+     */
+    public E get() {
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.removeFirst();
+    }
+
+    /**
+     * Returns all elements from the queue and clears it.
+     */
+    public Object[] getAll() {
+        Object[] res = new Object[list.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+        list.clear();
+        return res;
+    }
+
+
+    /**
+     * Peeks an element in the queue. Returned elements is not removed from the queue.
+     */
+    public E peek() {
+        return list.getFirst();
+    }
+
+    /**
+     * Returns <code>true</code> if queue is empty, otherwise <code>false</code>
+     */
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    /**
+     * Returns queue size.
+     */
+    public int size() {
+        return list.size();
+    }
+}
