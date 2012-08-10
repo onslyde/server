48c3e250c4978c5bd1e7de1fb1ef5cbd6ec12537
Wesley Hales <wesleyhales@gmail.com>
2012-08-10 14:41:05 -0400

    rough draft of a queue and timer

--- src/main/java/com/onslyde/service/PerformanceService.java
+++ src/main/java/com/onslyde/service/PerformanceService.java
@@ -1,15 +1,25 @@
 package com.onslyde.service;
 
+
+import com.onslyde.jms.PerfJMSClient;
 import com.onslyde.util.ClientEvent;
 import com.onslyde.util.SimpleQueue;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.Stateful;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.jms.*;
+import javax.jms.Queue;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
+
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.*;
@@ -20,42 +30,100 @@ import java.net.URLDecoder;
 import java.util.*;
 
 @Path("/performance")
-@RequestScoped
+@Singleton
 @Stateful
 public class PerformanceService {
 
-    @Inject
-    SimpleQueue simpleQueue;
+    //@Inject
+    //PerfJMSClient perfJMSClient;
+
+//    @PostConstruct
+//    public void init(){
+////        try {
+////            //perfJMSClient.init();
+////        } catch (Exception e) {
+////            e.printStackTrace();
+////        }
+//    }
+
+    public int incomingMsgs = 0;
+    private Timer timer = null;
+
+    private void startTimer(final int number){
+        if(timer == null){
+        if(number > 1){
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                public void run()  {
+                    // do stuff
+                    System.out.println("poll" + number);
+                    if(number >= 1){
+                        runTest();
+                        System.out.println("poll");
+                    }
+
+                }
+            }, 5000, 5000);
+            }
+        }
+    }
+
+    Map<String,String> tempMap = new HashMap<String,String>();
+
+//    @Resource(mappedName = "java:/ConnectionFactory")
+//    private ConnectionFactory connectionFactory;
+//    @Resource(mappedName = "java:jms/queue/test")
+//    pr
+// ivate Queue queue;
+
+    private static final String DEFAULT_USERNAME = "quickstartUser";
+    private static final String DEFAULT_PASSWORD = "quickstartPassword";
+    private static final String INITIAL_CONTEXT_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
+    private static final String PROVIDER_URL = "remote://localhost:4447";
+    private static final String DEFAULT_CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
+    Context context;
+    ConnectionFactory connectionFactory;
+    Destination destination;
+    Connection connection;
+    Session session;
+    MessageProducer messageProducer;
+    MessageConsumer consumer;
+
+    private void setupJMS() throws NamingException, JMSException {
+        if(context == null){
+            final Properties env = new Properties();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, INITIAL_CONTEXT_FACTORY);
+            env.put(Context.PROVIDER_URL, PROVIDER_URL);
+            env.put(Context.SECURITY_PRINCIPAL, DEFAULT_USERNAME);
+            env.put(Context.SECURITY_CREDENTIALS, DEFAULT_PASSWORD);
+            context = new InitialContext(env);
+            //if(connectionFactory == null){
+                String connectionFactoryString = System.getProperty("connection.factory", DEFAULT_CONNECTION_FACTORY);
+                connectionFactory = (ConnectionFactory) context.lookup("jms/RemoteConnectionFactory");
+            //}
+            destination = (Destination) context.lookup("jms/queue/test");
+            context.close();
+            connection = connectionFactory.createConnection("quickstartUser","quickstartPassword");
+            session = connection.createSession(false, Session.DUPS_OK_ACKNOWLEDGE);
+            messageProducer = session.createProducer(destination);
+            consumer = session.createConsumer(destination);
+            connection.start();
+        }
+        System.out.println("connection " + connection.toString());
+    }
 
     @GET
     @Path("/go")
     @Produces("text/html")
     public String go(@QueryParam("url") String url, @QueryParam("cached") String cached) {
-         Map<String,String> tempMap = new HashMap<String,String>();
-         tempMap.put("url",url);
-         tempMap.put("cached",cached);
-         simpleQueue.put(tempMap);
-         return runTest();
-    }
-    private String runTest(){
-        boolean running = false;
-        while(true){
+        String retVal = "";
+        String taskName = "";
 
-        if(!simpleQueue.isEmpty() && !running){
-            running = true;
-        Map<String,String> tempMap = (HashMap)simpleQueue.get();
-        String url = tempMap.get("url");
-        String cached = tempMap.get("cached");
-
+        tempMap.put("url",url);
+        tempMap.put("cached",cached);
+        startTimer(incomingMsgs);
         UUID random = UUID.randomUUID();
-        String taskName = "performance";
-//        try {
-//            param1AfterDecoding = URLDecoder.decode(url, "UTF-8");
-//            System.out.println(param1AfterDecoding);
-//        } catch (UnsupportedEncodingException e) {
-//            e.printStackTrace();
-//        }
-        //System.out.println("---" + url);
+        //if(incomingMsgs > 0){
         boolean keepgoing = false;
         try {
             URL urltemp = new URL(url);
@@ -70,41 +138,118 @@ public class PerformanceService {
             keepgoing = false;
         }
         Response.ResponseBuilder builder = null;
+
         if(keepgoing){
             if(cached.equals("true")){
                 taskName = "performancecache";
             }
-        try
-        {
 
-            for(int i = 0; i <= 5; i++) {
-                Process p=Runtime.getRuntime().exec("phantomjs --disk-cache=no confess-mod.js "+ url +" "+taskName+" json " + random.toString() );
-                //p.waitFor();
-                BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String line=reader.readLine();
-                while(line!=null)
-                {
-                //    System.out.println(line);
-                    line=reader.readLine();
-                }
+            try {
+                setupJMS();
+                TextMessage message = session.createTextMessage();
+                //for (int i = 0; i < 5; i++) {
+                    message.setText(url);
+                    System.out.println("===========" + incomingMsgs);
+                    messageProducer.send(message);
+                    incomingMsgs++;
+                    retVal = random.toString();
+                //}
+            } catch (JMSException e) {
+                e.printStackTrace();
+            } catch (NamingException e) {
+                e.printStackTrace();
             }
-        }
-        catch(IOException e1) {return "#fail";}
-        //catch(InterruptedException e2) {}
-
-            System.out.println("Done");
-            running = false;
         }else{
             System.out.println("Bad URL");
             return "#fail";
         }
 
+        return retVal;
+
+
+    }
+    public String runTest(){
+
+
+        //Map<String,String> tempMap = null;
+        String url = "";
+        String cached = "false";
+        String random = "";
+
+        String taskName = "performance";
+
+        if(incomingMsgs == 0){
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }else{
+
+    //        if(incomingMsgs > 1){
+    //            try {
+    //                Thread.sleep(10000 * incomingMsgs);
+    //            } catch (InterruptedException e) {
+    //                e.printStackTrace();
+    //            }
+    //            //runTest(url);
+    //        }
+
+            try {
+                setupJMS();
+                TextMessage message = (TextMessage) consumer.receive(1000);
+                System.out.println("Received message with content " + message + "---" + incomingMsgs);
+                url = message.getText();
+                incomingMsgs--;
+                //running = true;
+
+            } catch (NamingException e) {
+                e.printStackTrace();
+            } catch (JMSException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+                timer = null;
+            }
+
+
+            //System.out.println("---" + url);
+
+
+            try
+            {
+
+                for(int i = 0; i <= 5; i++) {
+                    Process p=Runtime.getRuntime().exec("phantomjs --disk-cache=no confess-mod.js "+ url +" "+taskName+" json " + random.toString() );
+                    //p.waitFor();
+                    BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    String line=reader.readLine();
+                    while(line!=null)
+                    {
+                    //    System.out.println(line);
+                        line=reader.readLine();
+                    }
+                }
+            }
+            catch(IOException e1) {return "#fail";}
+    //        catch(InterruptedException e2) {}
+
+            System.out.println("Done : " + incomingMsgs);
+
+                //if(incomingMsgs == 0){
+                    //runTest();
+                    //break;
+                //}
+
+                //running = false;
+
+        }
+
+
 
         //return builder.build();
         return "/rest/performance/report?uuid=" + random.toString();
-        }
-        }
-}
+
+
+    }
 
 
     @GET
