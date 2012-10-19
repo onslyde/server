9f82b7da886715304fdef27d385312ed0e73b027
Wesley Hales <wesleyhales@gmail.com>
2012-10-19 14:10:56 -0400

    images for remix12

---
+++ copyresources.sh
@@ -0,0 +1,7 @@
+cp -rf \
+/Users/wesleyhales/dev/onslyde/src/main/webapp/images \
+/Users/wesleyhales/dev/onslyde/src/main/webapp/js \
+/Users/wesleyhales/dev/onslyde/src/main/webapp/css \
+/Users/wesleyhales/dev/onslyde/src/main/webapp/remix12 \
+ /Users/wesleyhales/dev/onslyde/src/main/webapp/remix12.html \
+/Users/wesleyhales/dev/onslyde/target/onslyde-hosted/
