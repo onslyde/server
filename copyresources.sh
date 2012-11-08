0f46c855694e37ffacd1430f09d48d8cf107b9fe
Wesley Hales <wesleyhales@gmail.com>
2012-11-08 16:35:30 -0500

    updates for reveal.js

--- copyresources.sh
+++ copyresources.sh
@@ -2,6 +2,6 @@ cp -rf \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/images \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/js \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/css \
-/Users/wesleyhales/dev/onslyde/src/main/webapp/remix12 \
+/Users/wesleyhales/dev/onslyde/src/main/webapp/reveal \
  /Users/wesleyhales/dev/onslyde/src/main/webapp/remix12.html \
 /Users/wesleyhales/dev/onslyde/target/onslyde-hosted/
