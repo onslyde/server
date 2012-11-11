d5e003401281246a09499d7a36c974109c6ebd2a
Wesley Hales <wesleyhales@gmail.com>
2012-11-11 20:00:31 +0100

    slides for devoxx u

--- copyresources.sh
+++ copyresources.sh
@@ -3,6 +3,7 @@ cp -rf \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/js \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/css \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/reveal \
+/Users/wesleyhales/dev/onslyde/src/main/webapp/devoxx2012u \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/remix12 \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/remix12.html \
 /Users/wesleyhales/dev/onslyde/target/onslyde-hosted/
