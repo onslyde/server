061c6e3f7ce39096b1bf2aa2f633ac9ea1f40ab9
Wesley Hales <wesleyhales@gmail.com>
2013-02-15 13:16:32 -0500

    add reveal to global dirs and new devnexus slides

--- copyresources.sh
+++ copyresources.sh
@@ -5,6 +5,5 @@ cp -rf \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/reveal \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/devoxx2012u \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/devoxx12 \
-/Users/wesleyhales/dev/onslyde/src/main/webapp/remix12 \
-/Users/wesleyhales/dev/onslyde/src/main/webapp/remix12.html \
+/Users/wesleyhales/dev/onslyde/src/main/webapp/devnexus \
 /Users/wesleyhales/dev/onslyde/target/onslyde-hosted/
