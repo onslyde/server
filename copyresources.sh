5044687836f0b3b353a487321686f7bdb88cdd86
Wesley Hales <wesleyhales@gmail.com>
2013-02-16 12:17:12 -0500

    slide updates

--- copyresources.sh
+++ copyresources.sh
@@ -2,8 +2,5 @@ cp -rf \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/images \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/js \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/css \
-/Users/wesleyhales/dev/onslyde/src/main/webapp/reveal \
-/Users/wesleyhales/dev/onslyde/src/main/webapp/devoxx2012u \
-/Users/wesleyhales/dev/onslyde/src/main/webapp/devoxx12 \
 /Users/wesleyhales/dev/onslyde/src/main/webapp/devnexus \
 /Users/wesleyhales/dev/onslyde/target/onslyde-hosted/
