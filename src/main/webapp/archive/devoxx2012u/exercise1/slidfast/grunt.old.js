4b0c8e0fb5892ae07c2062760674ed90fefca831
Wesley Hales <wesleyhales@gmail.com>
2013-02-22 13:24:04 -0500

    archive a lot of talks and add video link to remote

---
+++ src/main/webapp/archive/devoxx2012u/exercise1/slidfast/grunt.old.js
@@ -0,0 +1,58 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* http://PROJECT_WEBSITE/\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'YOUR_NAME; Licensed MIT */'
+    },
+    lint: {
+      files: ['grunt.js', 'slidfast.js']
+    },
+//    qunit: {
+//      files: ['test/**/*.html']
+//    },
+    concat: {
+      dist: {
+        src: ['slidfast.js'],
+        dest: 'dist/slidfast.concat.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['slidfast.js'],
+        dest: 'dist/slidfast.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {}
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint concat min');
+
+};
