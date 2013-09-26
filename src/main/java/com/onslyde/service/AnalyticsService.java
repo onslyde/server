c4f7dc44d8248743ff947766ce037eb90b4bac1e
wesleyhales <wesleyhales@gmail.com>
2013-09-26 18:38:14 -0400

    remove this until auth is available

--- src/main/java/com/onslyde/service/AnalyticsService.java
+++ src/main/java/com/onslyde/service/AnalyticsService.java
@@ -58,20 +58,20 @@ public class AnalyticsService {
         return mySession;
     }
 
-    @GET
-    @Produces("application/json")
-    @Path("/remove/{session:[0-9][0-9]*}")
-    public String removeSessionVotes(@PathParam("session") int session){
-
-        try {
-            Session mySession = sessionHome.findById(session);
-            sgHome.removeBySessionId(mySession);
-            return "success!!!";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "fail!!";
-        }
-    }
+//    @GET
+//    @Produces("application/json")
+//    @Path("/remove/{session:[0-9][0-9]*}")
+//    public String removeSessionVotes(@PathParam("session") int session){
+//
+//        try {
+//            Session mySession = sessionHome.findById(session);
+//            sgHome.removeBySessionId(mySession);
+//            return "success!!!";
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return "fail!!";
+//        }
+//    }
 
     private class AllSessions{
         List<SessionSummary> sessionSummaries;
