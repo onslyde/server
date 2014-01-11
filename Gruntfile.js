767a0d485b89bbc424d0743ed57de1b374e52e6a
wesleyhales <wesleyhales@gmail.com>
2014-01-11 14:31:08 -0500

    add files to grunt build on server

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,10 @@ module.exports = function(grunt) {
             'src/main/webapp/panel/js/panel/fastclick.min.js'],
           'src/main/webapp/deck/js/deck/dist/onslyde-deck-1.0.0.js': ['src/main/webapp/deck/js/deck/libs/canvas2blob.js','src/main/webapp/deck/js/deck/libs/html2canvas.js','src/main/webapp/deck/js/deck/deck.js','src/main/webapp/deck/js/deck/onslyde-1.0.0.deck.js'],
           'src/main/webapp/deck/js/deck/dist/onslyde-core-1.0.0.js': ['src/main/webapp/deck/js/deck/onslyde-1.0.0.deck.js'],
-          'src/main/webapp/deck/js/deck/dist/onslyde-remote-1.0.0.js': ['src/main/webapp/deck/js/deck/gracefulWebSocket.js','src/main/webapp/deck/js/deck/remote.js']
+          'src/main/webapp/deck/js/deck/dist/onslyde-remote-1.0.0.js': ['src/main/webapp/deck/js/deck/gracefulWebSocket.js',
+                                                                        'src/main/webapp/deck/js/deck/remote.js',
+                                                                        'src/main/webapp/deck/js/deck/libs/gplus-oauth.js',
+                                                                        'src/main/webapp/deck/js/deck/libs/fastclick.min.js']
         }
       }
     },
